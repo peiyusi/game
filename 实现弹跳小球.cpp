726f6ecc547904bf2969b0c3af2d0ea3239fdb1b
peiyusi <1079986574@qq.com>
2017-08-22 17:32:10 +0800

    add first sample

---
+++ 实现弹跳小球.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main() {
+	int i, j;
+	int x = 1;
+	int y = 10;
+	
+	int top = 0;
+	int bottom = 10;
+	int left = 0;
+	int right = 20; 
+	int velocity_x = 1;
+	int velocity_y = 1;
+    
+	while (1) {
+	    x += velocity_x;
+		y += velocity_y;
+		
+		printf("--------------------\n");
+	    printf("|                  |\n");
+	    printf("--------------------\n"); 			
+		system("cls");
+		
+		for (i = 0; i < x; i++) {
+			printf("\n");
+		}
+		for (j = 0; j < y; j++) {
+			printf(" ");
+		}
+		printf("o");
+		printf("\n");
+		Sleep(50);
+		
+		if (x == top || x == bottom) {
+			velocity_x = -velocity_x;
+			printf("\a");
+		}
+		if (y == left || y == right) {
+			velocity_y = -velocity_y;
+			printf("\a");
+		}
+    }
+	
+	return 0;
+}
